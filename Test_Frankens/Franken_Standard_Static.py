134f26cbc81f1d76048f97faad6ae15eba960c01
RoryRose <74315186+RoryRose@users.noreply.github.com>
2020-11-18 09:42:48 +0000

    Created Working Standard Static Franken

--- Test_Frankens/Franken_Standard_Static.py
+++ Test_Frankens/Franken_Standard_Static.py
@@ -316,89 +316,29 @@ for i in range(1,NumOfTests+1):
     session.viewports['Viewport: 1'].view.setValues(nearPlane=0.00469895, 
         farPlane=0.00712223, width=0.00188493, height=0.000775242, 
         viewOffsetX=0.000207475, viewOffsetY=-0.000125154)
-    mdb.models[ModelName].StaticStep(name=SName2, previous='Initial')
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName2)
-    mdb.models[ModelName].StaticStep(name=SName, previous=SName2)
+    mdb.models[ModelName].StaticStep(name=SName, previous='Initial')
     session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName)
-    mdb.models[ModelName].StaticStep(name=SName3, previous=SName)
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName3)
-    ##delete automatically created output requests#
-    #del mdb.models[ModelName].fieldOutputRequests['F-Output-1']
-    #del mdb.models[ModelName].historyOutputRequests['H-Output-1']
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName2)
-    session.viewports['Viewport: 1'].partDisplay.setValues(mesh=OFF)
-    session.viewports['Viewport: 1'].partDisplay.meshOptions.setValues(
-        meshTechnique=OFF)
-    session.viewports['Viewport: 1'].partDisplay.geometryOptions.setValues(
-        referenceRepresentation=ON)
-    p1 = mdb.models[ModelName].parts[PrtName]
-    session.viewports['Viewport: 1'].setValues(displayedObject=p1)
-    #create sets#
-    p = mdb.models[ModelName].parts[PrtName]
-    v = p.vertices
-    verts = v.getSequenceFromMask(mask=('[#10000 ]', ), )
-    p.Set(vertices=verts, name=DiskFreeName)
-    p = mdb.models[ModelName].parts[PrtName]
-    v = p.vertices
-    verts = v.getSequenceFromMask(mask=('[#1 ]', ), )
-    p.Set(vertices=verts, name=EndFreeName)
-    a = mdb.models[ModelName].rootAssembly
     #regenerate part#
     a.regenerate()
     session.viewports['Viewport: 1'].setValues(displayedObject=a)
     #create history output request#
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName2)
-    regionDef=mdb.models[ModelName].rootAssembly.allInstances[InstName].sets[DiskFreeName]
-    mdb.models[ModelName].HistoryOutputRequest(name='H-Output-1', 
-        createStepName=SName2, variables=('U3', ), region=regionDef, 
-        sectionPoints=DEFAULT, rebar=EXCLUDE)
     session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName)
     regionDef=mdb.models[ModelName].rootAssembly.sets[CentFreeName]
     mdb.models[ModelName].HistoryOutputRequest(name='H-Output-2', 
         createStepName=SName, variables=('U3', ), region=regionDef, 
         sectionPoints=DEFAULT, rebar=EXCLUDE)
-    regionDef=mdb.models[ModelName].rootAssembly.allInstances[InstName].sets[EndFreeName]
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName3)
-    regionDef=mdb.models[ModelName].rootAssembly.allInstances[InstName].sets[EndFreeName]
-    mdb.models[ModelName].HistoryOutputRequest(name='H-Output-3', 
-        createStepName=SName3, variables=('U3', ), region=regionDef, 
-        sectionPoints=DEFAULT, rebar=EXCLUDE)
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(loads=ON, bcs=ON, 
-        predefinedFields=ON, connectors=ON, adaptiveMeshConstraints=OFF)
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName2)
     #create forces#
-    a = mdb.models[ModelName].rootAssembly
-    region = a.instances[InstName].sets[DiskFreeName]
-    mdb.models[ModelName].ConcentratedForce(name='Load-1', 
-        createStepName=SName2, region=region, cf3=-1.0, 
-        distributionType=UNIFORM, field='', localCsys=None)
     session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName)
     a = mdb.models[ModelName].rootAssembly
     region = a.sets[CentFreeName]
     mdb.models[ModelName].ConcentratedForce(name='Load-2', createStepName=SName, 
         region=region, cf3=-1.0, distributionType=UNIFORM, field='', 
         localCsys=None)
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName3)
-    a = mdb.models[ModelName].rootAssembly
-    region = a.instances[InstName].sets[EndFreeName]
-    mdb.models[ModelName].ConcentratedForce(name='Load-3', createStepName=SName3, 
-        region=region, cf3=-1.0, distributionType=UNIFORM, field='', 
-        localCsys=None)
-    #create BC's#
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName2)
-    a = mdb.models[ModelName].rootAssembly
-    region = a.sets[EncName]
-    mdb.models[ModelName].EncastreBC(name='BC-1', createStepName=SName2, 
-        region=region, localCsys=None)
     session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName)
+    #create BCs#
     a = mdb.models[ModelName].rootAssembly
     region = a.sets[EncName]
     mdb.models[ModelName].EncastreBC(name='BC-2', createStepName=SName, 
-        region=region, localCsys=None)
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step=SName3)
-    a = mdb.models[ModelName].rootAssembly
-    region = a.sets[EncName]
-    mdb.models[ModelName].EncastreBC(name='BC-3', createStepName=SName3, 
         region=region, localCsys=None)
     session.viewports['Viewport: 1'].assemblyDisplay.setValues(loads=OFF, bcs=OFF, 
         predefinedFields=OFF, connectors=OFF)
@@ -425,21 +365,11 @@ for i in range(1,NumOfTests+1):
         name=ODBName+JobName+'.odb')
     session.viewports['Viewport: 1'].setValues(displayedObject=o3)
     odb = session.odbs[JobName+'.odb']#odb = session.odbs[ODBName+JobName+'.odb']
-    xy0 = session.XYDataFromHistory(name='Data-1', odb=odb, 
-        outputVariableName='Spatial displacement: U3 at Node 3 in NSET NEAR_END', 
-        steps=(SName3, ), )
-    c0 = session.Curve(xyData=xy0)
-    xy1 = session.XYDataFromHistory(name='Data-2', odb=odb, 
+    xy1 = session.XYDataFromHistory(name='Data-1', odb=odb, 
         outputVariableName='Spatial displacement: U3 at Node 7 in NSET CENTER_OF_FREE_END', 
         steps=(SName, ), )
     c1 = session.Curve(xyData=xy1)
-    xy2 = session.XYDataFromHistory(name='Data-3', odb=odb, 
-        outputVariableName='Spatial displacement: U3 at Node 17 in NSET NEAR_DISK', 
-        steps=(SName2, ), )
-    c2 = session.Curve(xyData=xy2)
     #create output .rpt file#
     x0 = session.xyDataObjects['Data-1']
-    x1 = session.xyDataObjects['Data-2']
-    x2 = session.xyDataObjects['Data-3']
-    session.writeXYReport(fileName=RPTName+'.rpt', xyData=(x0, x1, x2))
+    session.writeXYReport(fileName=RPTName+'.rpt', xyData=(x0))
     time.sleep(2)
