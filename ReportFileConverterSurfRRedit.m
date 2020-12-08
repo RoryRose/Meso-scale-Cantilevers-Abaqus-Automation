f41bb4715c2c462d7973ed10ec6584b11b508bfc
IonicRob <62338052+IonicRob@users.noreply.github.com>
2020-12-08 14:39:41 +0000

    Update ReportFileConverterSurfRRedit.m

--- ReportFileConverterSurfRRedit.m
+++ ReportFileConverterSurfRRedit.m
@@ -1,24 +1,38 @@
 
 % function outputmatrix = ReportFileConverterSurf
 
-    [file, path] = uigetfile('Select the report file:');
-    filename = fullfile(path,file);
+%% Settings
+clc
+clear
+close all
+ScatterORInt = 'Int'; % 'Int'
+PlotTF = false;
+
+%% Reading Data
+[file, path] = uigetfile('Select the report file:','MultiSelect','on');
+
+if isa(file,'char') == true
+    file = cellstr(file);
+end
+
+outPutArray = nan(length(file),1);
+NameArray = transpose(string(file));
+
+for FileNum = 1:length(file)
+
+    filename = fullfile(path,file{FileNum});
     Lines_1 = py2string(filename,'r');
     Lines_2a = Lines_1(24:end);
     Lines_2 = replace(Lines_2a,'No Value','NaN');
     endline = find((Lines_2 == ''), 1, 'first');
 
     for i = 1:endline-1
-        disp(i)
         current_row = Lines_2(i);
-%         disp(current_row);
         Lines_3 = split(current_row,' ',2);
         curr_num = 1;
         for j = 1:length(Lines_3)
-    %         disp(j);
             curr_Val = Lines_3(j); 
             if strcmp(curr_Val,"") == false
-%                 disp(curr_Val)
                 if curr_num == 1
                     curr_line = string(curr_Val);
                 else
@@ -26,7 +40,6 @@
                 end
                 curr_num = curr_num+1;
             end
-%             disp(curr_line)
         end
 
         if i == 1
@@ -38,132 +51,142 @@
         Lines_5 = split(Lines_4,' ',2);
         Lines_6 = double(Lines_5);
     end
+    fprintf('%d Done!\n',FileNum);
+    %% PostProcessing
 
-%% PostProcessing
-
-clc
-clearvars -except Lines_6
-close all
-
-Lines_7pre = Lines_6(~isnan(Lines_6(:,2)),:);
-MissingData = Lines_7pre(isnan(Lines_7pre(:,6)),:);
-% Lines_7 = Lines_7pre;
-Lines_7 = Lines_7pre(~isnan(Lines_7pre(:,6)),:);
+    Lines_7pre = Lines_6(~isnan(Lines_6(:,2)),:);
+    MissingData = Lines_7pre(isnan(Lines_7pre(:,6)),:);
+    % Lines_7 = Lines_7pre;
+    Lines_7 = Lines_7pre(~isnan(Lines_7pre(:,6)),:);
 
 
-shp = alphaShape(Lines_7(:,3),Lines_7(:,4));
-shp.Alpha = 1.50*10^-5;
-inmask1 = inpolygon(Lines_7(:,3),Lines_7(:,4),shp.Points(:,1),shp.Points(:,2));
+    shp = alphaShape(Lines_7(:,3),Lines_7(:,4));
+    shp.Alpha = 1.50*10^-5;
+    inmask1 = inpolygon(Lines_7(:,3),Lines_7(:,4),shp.Points(:,1),shp.Points(:,2));
 
 
-COOR1 = Lines_7(:,3);
-COOR2 = Lines_7(:,4);
-COOR3 = Lines_7(:,5);
-SMises = Lines_7(:,6);
-S11 = Lines_7(:,7);
-S22 = Lines_7(:,8);
-S33 = Lines_7(:,9);
-S12 = Lines_7(:,10);
-S13 = Lines_7(:,11);
-S23 = Lines_7(:,12);
+    COOR1 = Lines_7(:,3);
+    COOR2 = Lines_7(:,4);
+    COOR3 = Lines_7(:,5);
+    SMises = Lines_7(:,6);
+    S11 = Lines_7(:,7);
+    S22 = Lines_7(:,8);
+    S33 = Lines_7(:,9);
+    S12 = Lines_7(:,10);
+    S13 = Lines_7(:,11);
+    S23 = Lines_7(:,12);
 
-Quantity2Plot = S11;
+    Quantity2Plot = S11;
 
-MinXY = min(min(COOR1),min(COOR2));
-MaxXY = max(max(COOR1),max(COOR2));
-
-
-x = COOR1;
-y = COOR2;
-v = Quantity2Plot;
-
-npts = length(x); % Number of points
-% round((length(x)^0.5))
-
-[~,IndXMax] = max(x);
-
-ScatterORInt = 'Int'; % 'Int'
-
-switch ScatterORInt
-    case 'Int'
-        % [xg,yg] = meshgrid(linspace(MinXY,MaxXY,100));
-        A=600;%number of columns/rows in the grid
-        [xg,yg] = meshgrid(linspace(min(x),max(x),A),linspace(min(y),max(y),A));
-        F = scatteredInterpolant(x,y,v);
-        vg = F(xg,yg);
-
-        b = boundary(x,y);
-        inmask = inpolygon(xg(:),yg(:), x(b),y(b));
-        vg(~inmask) = nan;
-        xg_new = xg(~inmask);
-        yg_new = yg(~inmask);
-        % MinXY = min(min(x),min(y));
-        % MaxXY = max(max(x),max(y));
-        % [xg,yg] = meshgrid(linspace(MinXY,MaxXY,300));
-        % F = scatteredInterpolant(x,y,v);
-        % vg = F(xg,yg);
-        h = pcolor(xg,yg,vg);
-        h.EdgeColor = 'none';
-        c = colorbar;
-        xlimvec = [min(xg(inmask)),max(xg(inmask))];
-        ylimvec = [min(yg(inmask)),max(yg(inmask))];
-        xlim(xlimvec);
-        ylim(ylimvec);
+    MinXY = min(min(COOR1),min(COOR2));
+    MaxXY = max(max(COOR1),max(COOR2));
 
 
-        % dt = delaunayTriangulation(x,y);
-        % [K,v] = convexHull(dt);
-        % xp = x(K);
-        % yp = y(K);
-        % inmask = inpolygon(x(:),y(:), xp,yp);
-        % v(~inmask) = nan;
-        % % h = trisurf(dt.ConnectivityList,x,y,zeros(length(v),1),v);
-        % h = trisurf(K,x,y,zeros(length(v),1),v);
-        % h.FaceColor = 'interp';
-        % % h.EdgeColor = 'none';
-        % view(2)
-        % colorbar
-        hold on
-        % plot(shp);
-    otherwise
-%         scatter(x,y,36,v,'filled');
-        scatter(x,y,36,[0,0,0],'x');
-        c = colorbar;
+    x = COOR1;
+    y = COOR2;
+    v = Quantity2Plot;
+
+    npts = length(x); % Number of points
+    % round((length(x)^0.5))
+
+    [~,IndXMax] = max(x);
+
+    switch ScatterORInt
+        case 'Int'
+            % [xg,yg] = meshgrid(linspace(MinXY,MaxXY,100));
+            A=600;%number of columns/rows in the grid
+            [xg,yg] = meshgrid(linspace(min(x),max(x),A),linspace(min(y),max(y),A));
+            F = scatteredInterpolant(x,y,v);
+            vg = F(xg,yg);
+
+            b = boundary(x,y);
+            inmask = inpolygon(xg(:),yg(:), x(b),y(b));
+            vg(~inmask) = nan;
+            xg_new = xg(~inmask);
+            yg_new = yg(~inmask);
+            % MinXY = min(min(x),min(y));
+            % MaxXY = max(max(x),max(y));
+            % [xg,yg] = meshgrid(linspace(MinXY,MaxXY,300));
+            % F = scatteredInterpolant(x,y,v);
+            % vg = F(xg,yg);
+
+            if PlotTF == true
+                h = pcolor(xg,yg,vg);
+                h.EdgeColor = 'none';
+                c = colorbar;
+                xlimvec = [min(xg(inmask)),max(xg(inmask))];
+                ylimvec = [min(yg(inmask)),max(yg(inmask))];
+                xlim(xlimvec);
+                ylim(ylimvec);
+            end
+
+            % dt = delaunayTriangulation(x,y);
+            % [K,v] = convexHull(dt);
+            % xp = x(K);
+            % yp = y(K);
+            % inmask = inpolygon(x(:),y(:), xp,yp);
+            % v(~inmask) = nan;
+            % % h = trisurf(dt.ConnectivityList,x,y,zeros(length(v),1),v);
+            % h = trisurf(K,x,y,zeros(length(v),1),v);
+            % h.FaceColor = 'interp';
+            % % h.EdgeColor = 'none';
+            % view(2)
+            % colorbar
+            hold on
+            % plot(shp);
+        otherwise
+    %         scatter(x,y,36,v,'filled');
+            if PlotTF == true
+                scatter(x,y,36,[0,0,0],'x');
+                c = colorbar;
+            end
+    end
+
+    if PlotTF == true
+        % c.Ticks = [linspace(round(min(v)),round(max(v)),10),0];
+
+        % plot(xg(1,:),vg(300,:));
+
+        caxis([round(min(vg(300,:))) round(max(vg(300,:)))])
+
+        % A = min(vg(300,:));
+        % B = min(v);
+    end
+
+    % end
+    % RPTRead('C:\Users\rober\Documents\GitHub\Meso-scale-Cantilevers-Abaqus-Automation\Example Rpt files\Direct_Dynamicstress-dist.rpt');
+    %% Find angle of beam
+    ACOOR1 = MissingData(:,3)+MissingData(:,14); %deformed coordinate x1
+    ACOOR2 = MissingData(:,4)+MissingData(:,15);%deformed coordinate x2
+    ACOOR3 = MissingData(:,5) + MissingData(:,16);%deformed coordinate x3
+    Angle = atan((ACOOR3(2,1)-ACOOR3(1,1))/(ACOOR1(2,1)-ACOOR1(1,1)));% *180/pi%angle in degrees
+    grad = (ACOOR3(2,1)-ACOOR3(1,1))/(ACOOR1(2,1)-ACOOR1(1,1))%angle in degrees
+    %% creating histogram of stres
+    Grid_Area = (max(x)-min(x))*(max(y)-min(y))/(A^2);
+    countvals = nnz(~isnan(xg));
+    idx = ~isnan(vg);
+    totalArea = countvals*Grid_Area;
+    [N,edges] = histcounts(vg(idx)./Angle, 'Normalization', 'probability');
+    xbar = edges(1:numel(N)) + mean(diff(edges))/2;
+    figure
+    bar(xbar, N)
+    grid
+    yt = get(gca, 'YTick'); 
+    ytix = linspace(min(yt), max(yt), 10);
+    %set(gca, 'YTick',ytix, 'YTickLabel',fix(ytix*totalArea/max(yt)))
+    ytickformat('%.2f')
+    xlabel('Stress/angle(Pa/Rad)')
+    ylabel('Fraction of Area')
+
+    [maxValue,maxIndex] = max(N);
+    maxPaPerRad = abs(mean([edges(maxIndex),edges(maxIndex+1)]));
+    outPutArray(FileNum) = maxPaPerRad;
+    pause(3);
+    close all
 end
 
-% c.Ticks = [linspace(round(min(v)),round(max(v)),10),0];
+outPutTable = table(NameArray,outPutArray);
 
-% plot(xg(1,:),vg(300,:));
-
-caxis([round(min(vg(300,:))) round(max(vg(300,:)))])
-
-% A = min(vg(300,:));
-% B = min(v);
-
-% end
-% RPTRead('C:\Users\rober\Documents\GitHub\Meso-scale-Cantilevers-Abaqus-Automation\Example Rpt files\Direct_Dynamicstress-dist.rpt');
-%% Find angle of beam
-ACOOR1 = MissingData(:,3)+MissingData(:,14); %deformed coordinate x1
-ACOOR2 = MissingData(:,4)+MissingData(:,15);%deformed coordinate x2
-ACOOR3 = MissingData(:,5) + MissingData(:,16);%deformed coordinate x3
-Angle = atan((ACOOR3(2,1)-ACOOR3(1,1))/(ACOOR1(2,1)-ACOOR1(1,1)))*180/pi%angle in degrees
-grad = (ACOOR3(2,1)-ACOOR3(1,1))/(ACOOR1(2,1)-ACOOR1(1,1))%angle in degrees
-%% creating histogram of stres
-Grid_Area = (max(x)-min(x))*(max(y)-min(y))/(A^2);
-countvals = nnz(~isnan(xg));
-idx = ~isnan(vg);
-totalArea = countvals*Grid_Area;
-[N,edges] = histcounts(vg(idx)./grad, 'Normalization', 'probability');
-xbar = edges(1:numel(N)) + mean(diff(edges))/2;
-figure
-bar(xbar, N)
-grid
-yt = get(gca, 'YTick'); 
-ytix = linspace(min(yt), max(yt), 10);
-%set(gca, 'YTick',ytix, 'YTickLabel',fix(ytix*totalArea/max(yt)))
-ytickformat('%.2f')
-xlabel('Stress/angle(Pa/gradient)')
-ylabel('Fraction of Area')
 %%
 function LinesString = py2string(filename,permission)
     % Helped by the following URLs:
